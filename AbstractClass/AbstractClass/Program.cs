c3c86609500a2946808accbf96476a62b71d01e2
Carl Matthew Hamilton <info@chtsi.uk>
2017-08-11 22:29:57 +0100

    update

---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+
+    /// <summary>
+    /// Abstract definition of any type of car
+    /// </summary>
+    public abstract class Car
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// The cars wheel diameter as it is unknown
+        /// </summary>
+        public abstract int WheelDiameterInches { get; }
+
+
+        /// <summary>
+        /// Indicates if the handbrake is released
+        /// </summary>
+
+        public bool HandBrakeReleased { get; set; } = false;
+
+        #endregion
+        #region Public Methods
+        public virtual void RadioOn()
+        {
+            Console.WriteLine("Turn the radio on");
+        }
+
+        /// <summary>
+        /// Attempts to release the cars handbrake
+        /// </summary>
+        public virtual void ReleaseHandbrake()
+        {
+            // Release the handbrake
+            HandBrakeReleased = true;
+
+            Console.WriteLine("Sucessfully release the handbrake");
+        }
+
+        /// <summary>
+        /// Apply gas to drive the car
+        /// </summary>
+
+        public virtual void Drive()
+        {
+            ReleaseHandbrake();
+            if (HandBrakeReleased)
+                Console.WriteLine("Pressed the gas peddle");
+            else
+                Console.WriteLine("Cannot move handbrake jammed");
+
+        }
+
+        public float GearboxOutputSpeedRpm()
+        {
+            return 1000;
+        }
+        public float CalculateSpeedMph()
+        {
+            //if handbrake on we aren't moving
+            if (!HandBrakeReleased)
+                return 0;
+
+            var InchesPerRev = (float)(Math.PI * WheelDiameterInches);
+            var InchesPerMinute = InchesPerRev * GearboxOutputSpeedRpm();
+            var mph = InchesPerMinute * 60 * 0.0000157828;
+
+            return (float)mph;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// This will wind the car window down
+        /// </summary>
+
+        public abstract void WindWindowDown();
+
+
+        /// <summary>
+        /// Moves the seat forward
+        /// </summary>
+        public abstract void MoveSeatForward();
+
+    }
+    public class BudgetCar : Car
+    {
+        public override int WheelDiameterInches => 16;
+
+        public override void MoveSeatForward()
+        {
+            Console.WriteLine("Move seat forward using hand lever");
+        }
+
+        public override void WindWindowDown()
+        {
+            Console.WriteLine("Wound window down slowly using winder");
+        }
+    }
+
+    public class SportsCar : Car
+    {
+        public override int WheelDiameterInches => 22;
+
+        public override void MoveSeatForward()
+        {
+            Console.WriteLine("Move seat forward using button");
+        }
+
+        public override void WindWindowDown()
+        {
+            Console.WriteLine("Pressed button to adjust window");
+        }
+    }
+
+    public class BrokenBudgetCar : BudgetCar
+    {
+        public override void ReleaseHandbrake()
+        {
+            //Handbrake stuck always on
+            HandBrakeReleased = false;
+
+            Console.WriteLine("!! Handbrake failed to release !!");
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Budget car");
+            Console.WriteLine("----------------");
+            var budgetCar = new BudgetCar();
+            doStuffToCar(budgetCar);
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Sports car");
+            Console.WriteLine("----------------");
+            var sportsCar = new SportsCar();
+            doStuffToCar(sportsCar);
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Budget car");
+            Console.WriteLine("----------------");
+            var budgetCar2 = new BrokenBudgetCar();
+            doStuffToCar(budgetCar2);
+            Console.WriteLine("\n");
+
+
+            Console.ReadLine();
+        }
+
+        private static void doStuffToCar(Car car)
+        {
+            car.MoveSeatForward();
+            car.RadioOn();
+            car.WindWindowDown();
+            car.Drive();
+
+            Console.WriteLine($"Driving at {car.CalculateSpeedMph()}mph with {car.WheelDiameterInches}\" rims");
+        }
+    }
+}
---
+++ EnumsExample/EnumsExample/Program.cs
@@ -0,0 +1,134 @@
+﻿using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumsExample
+{
+
+    /// <summary>
+    ///  Enum is used to store values - consider like a variable.
+    /// </summary>
+
+    public enum myEnums
+    {
+
+        // We set constant list of values this is an enum
+        Unknown = 0,
+        MyValue1 = 1,
+        MyValue2 = 2,
+        MyValue3 = 3
+    }
+
+    public enum AnimationDirection
+    {
+        Unknown = 0,
+        Left = 1,
+        Right = 2,
+        Up = 3,
+        Down = 4
+    }
+    [Flags]
+    public enum userPermissions
+    {
+        ReadBlogs = 0x1,
+        WriteBlogs =0x2,
+        DeleteBlogs = 0x4,
+        CreateUsers = 0x8,
+        A = 0x10,
+        B = 0x20,
+        C = 0x40,
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // What are enums?
+            int i = default(int);
+            var a = myEnums.MyValue1;
+            var L = AnimationDirection.Left;
+            // Different Base Types
+            var myInt = (int)myEnums.MyValue2;
+            var myDInt = (int)AnimationDirection.Up;
+
+            // Where to use them?
+
+            Animate(AnimationDirection.Right);
+
+            // Casting to/from int / type
+
+            var LAsInt = (int)L;
+            var LBackAsItSelf = (AnimationDirection)LAsInt;
+
+            var directCreate = (AnimationDirection)4;
+
+            // Parsing from string name
+            var enumAsName = L.ToString();
+
+
+            if (Enum.TryParse<AnimationDirection>(enumAsName, out AnimationDirection enumFromString))
+            {
+                Console.WriteLine($"Successfully converted to {enumFromString}");
+            }
+            else
+                Console.WriteLine($"Failed to convert {enumAsName}");
+
+
+
+
+
+            // Enumerating the enum
+
+            foreach (var val in Enum.GetValues(typeof(AnimationDirection)))
+                 Console.WriteLine(val);
+            foreach (var val in Enum.GetNames(typeof(AnimationDirection)))
+                Console.WriteLine(val);
+            var name1 = Enum.GetName(typeof(AnimationDirection), L);
+
+            // Flags
+
+            var permissions = userPermissions.ReadBlogs | userPermissions.CreateUsers;
+
+            var nextPermission = (userPermissions)37;
+
+            if((permissions & userPermissions.DeleteBlogs) == userPermissions.DeleteBlogs)
+            {
+                // use case if they have permission to delete blogs.
+            }
+
+            Console.Write(permissions);
+            Console.WriteLine($"\n---------");
+            Console.Write(nextPermission);
+
+
+
+            Console.Read();
+        }
+        public static void Animate(AnimationDirection direction
+            )
+        {
+            switch (direction)
+            {
+                case AnimationDirection.Left:
+                    Console.WriteLine("Animating Left...");
+                    break;
+                case AnimationDirection.Right:
+                    Console.WriteLine("Animating Right...");
+                    break;
+                case AnimationDirection.Up:
+                    Console.WriteLine("Animating Up...");
+                    break;
+                case AnimationDirection.Down:
+                    Console.WriteLine("Animating Down...");
+                    break;
+                default:
+                    Console.WriteLine("I don't know what to do");
+                    break;
+
+            }
+        }
+    }
+}
