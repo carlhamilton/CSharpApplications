aaf87ced4b82cc968ec59e161a7ab13955240b18
Carl Matthew Hamilton <info@chtsi.uk>
2017-08-16 18:58:48 +0100

    WhiteBoardInterview
    WhiteBoardInterview

---
+++ StringTest/StringTest/stringloop.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+namespace StringTest
+{
+    class stringloop
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hello, World";
+            //loop runs through the string
+            for (int i = 0; i < str.Length; i++)
+            {
+
+                Console.WriteLine(str[i] + " Index is at: " + str.IndexOf(str[i]));
+
+
+
+
+            }
+
+            //counts the total number of elements used on a string
+            int count = str.Count();
+            //outputs the answer
+            Console.WriteLine("There are {0} Letters in the string", count-1);
+            Console.ReadKey();
+        }
+    }
+}
