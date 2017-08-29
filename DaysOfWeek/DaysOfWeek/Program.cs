2891d81b91e33a5b74bbf3aead6844471e06975c
Carl Matthew Hamilton <info@chtsi.uk>
2017-08-29 16:00:31 +0100

    added new

---
+++ DaysOfWeek/DaysOfWeek/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string monday = "Monday";
+            string[] daysOfWeek = //type of []
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            daysOfWeek[5] = "partyDay";
+            foreach (string day in daysOfWeek)
+            {
+                Console.WriteLine(day);
+            }
+
+            var sb = new StringBuilder();
+            for (int i = 0; i < daysOfWeek.Length; i++)
+            {
+                sb.Append(daysOfWeek[i]);
+                if ((i < daysOfWeek.Length - 2))
+                {
+                    sb.Append(", ");
+                }
+                else if ((i == daysOfWeek.Length - 2))
+                    sb.Append(" and ");
+
+            }
+            Console.WriteLine(sb.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DeclareArray/DeclareArray/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeclareArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1;
+            int[] x2;
+
+            int x3 = 5;
+            int[] x4 = new int[5];
+            Console.WriteLine(x4[0]);
+
+            int[] x5 = {1, 4, 9, 16, 25};
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
