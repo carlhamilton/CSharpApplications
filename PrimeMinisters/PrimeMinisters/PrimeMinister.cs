6b01f5f5575a1028e2c6d0b0f7d29b89d190527b
Carl Hamilton <carl.hamilton@chtsi.co.uk>
2019-08-07 15:33:26 +0100

    Update

--- PrimeMinisters/PrimeMinisters/PrimeMinister.cs
+++ PrimeMinisters/PrimeMinisters/PrimeMinister.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//Primeminister calculation.
 namespace PrimeMinisters
 {
     class PrimeMinister
